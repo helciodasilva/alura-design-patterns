0387affb2e52b63040aec8e1174557934b1084f9
helciodasilva <helciodasilva@gmail.com>
2020-10-26 09:28:21 -0300

    Aula 4: Comportamentos compostos por outros comportamentos e o Decorator

--- src/main/java/br/com/alura/curso/design/patterns/imposto/TesteDeImpostos.java
+++ src/main/java/br/com/alura/curso/design/patterns/imposto/TesteDeImpostos.java
@@ -2,16 +2,14 @@ package br.com.alura.curso.design.patterns.imposto;
 
 public class TesteDeImpostos {
 
-	public static void main(String[] args) {
-		ISS iss = new ISS();
-		ICMS icms = new ICMS();
-		
-		Orcamento orcamento = new Orcamento(500.0);
-		
-		CalculadorDeImpostos calculador = new CalculadorDeImpostos();
-		
-		calculador.realizaCalculo(orcamento, iss);
-		calculador.realizaCalculo(orcamento, icms);
-	}
+    public static void main(String[] args) {
+        Imposto iss = new ISS();
+
+        Orcamento orcamento = new Orcamento(500.0);
+
+        double valor = iss.calcula(orcamento);
+
+        System.out.println(valor);
+    }
 	
 }
